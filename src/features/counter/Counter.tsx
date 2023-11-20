d014ca844801fda47d0a4b8c33a150c3877a1cc0
hk12 <kimhuyhuynh30@gmail.com>
2023-11-20 22:18:46 +0700

    fix....

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -60,7 +60,7 @@ export function Counter() {
           className={styles.button}
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
-          Add If Odd
+          Tao dep trai
         </button>
       </div>
     </div>
